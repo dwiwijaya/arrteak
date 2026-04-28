ef69f73d532094b6369ce56cd81d176173508fb8
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2026-04-28 07:19:27 +0700

    fix: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://kayuart.id',
+  devToolbar: {
+    enabled: false,
+  },
   image: {
     remotePatterns: [
       {
